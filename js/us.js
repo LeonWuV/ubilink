06884f2c6270b8fa7a79e5ca1e68178996e8860f
wu xiao long <wuxiaolomng802@163.com>
2016-11-04 18:27:35 +0800

    11.04,third

--- js/us.js
+++ js/us.js
@@ -6,38 +6,25 @@ $(function(){
 		var innerWidth1 = document.documentElement.clientWidth;
 		//让轮播图大小适应不同的浏览器第一屏
 		$(".header").css({"height":innerHeight1,"width":innerWidth1});
-		$(".banner").css({"height":innerHeight1,"width":innerWidth1});
-		$(".lastimg").css("height",innerHeight1);
 	})();
-	//首页轮播图
-	var mySwiper = new Swiper ('.swiper-container', {
-	    loop: true,
-	    
-	    // 分页器
-	    pagination: '.swiper-pagination',
-	    //分页器是否可以点击
-	    paginationClickable :true,
-	    // 如果需要前进后退按钮
-	    nextButton: '.swiper-button-next',
-	    prevButton: '.swiper-button-prev',
-		
-		//滑动间隔时间
-	    autoplay : 3000,
-	    //滑动一张的时间
-		speed:500
-    });
-    //导航栏下划线移动
+	
+	 //导航栏下划线移动
     	//鼠标放上去下划线跑动
     $(".nav-ul li").on("mouseover",function(){
     	$(".underline").stop().animate({
     		"margin-left":$(this).index()*132
     	},300);   	
     });
-    	//如果不点击，鼠标离开下划线回到第一个初始位置
+    	//如果不点击，鼠标离开下划线回到第初始位置
      $(".nav-ul li").on("mouseout",function(){
     	$(".underline").stop().animate({
-    		"margin-left":0
+    		"margin-left":396
     	},600);   	
     });
-  
+    
+    
+    
+    
+	
+	
 });
